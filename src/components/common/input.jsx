f1d8efcfa1c26244c60520c1d6fbf796fda5c824
Kunal Rajput <kunal.rajput2511@gmail.com>
2023-04-13 12:46:37 +0530

    Vidly App before adding JOI validation library

---
+++ src/components/common/input.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+const Input = ({ name, label, value, onChange, error }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        id={name}
+        name={name}
+        className="form-control"
+      />
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
+export default Input;
